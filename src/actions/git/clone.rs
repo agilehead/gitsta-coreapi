85b79d4642d2be20f6fdde4f69ad75ab65387bac
Jeswin <jeswinpk@agilehead.com>
2020-04-02 20:23:40 +0530

    Working native libs after splitting the projects

--- src/actions/git/clone.rs
+++ src/actions/git/clone.rs
@@ -7,8 +7,8 @@ struct Repo<'a> {
 }
 
 pub async fn clone_over_http<'a>(args: &str) -> Result<String, String> {
-  // let url = "https://github.com/alexcrichton/git2-rs";
-  // let repo = Repository::clone(url, "/path/to/a/repo");
+  let url = "https://github.com/alexcrichton/git2-rs";
+  let repo = Repository::clone(url, "/path/to/a/repo");
   
   Ok(r#"{ result: true }"#.to_owned())
 }
