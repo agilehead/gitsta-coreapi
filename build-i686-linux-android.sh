d67e5261030ef2803da3394b7434bfb8822b940f
Jeswin <jeswinpk@agilehead.com>
2020-04-03 18:57:56 +0530

    Delete i686 build

--- build-i686-linux-android.sh
+++
@@ -1,7 +0,0 @@
-BASEDIR=$(dirname "$0")
-
-export OPENSSL_LIB_DIR=`realpath "$BASEDIR"/deps/openssl`
-export OPENSSL_INCLUDE_DIR=`realpath "$BASEDIR"/deps/openssl/include`
-
-echo Compiling for i686-linux-android...
-cargo ndk --target i686-linux-android --android-platform 28 -- build --release
