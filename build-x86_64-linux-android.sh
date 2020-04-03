64a37c299fbe599d5acb645a1d1ef4327e2915ae
Jeswin <jeswinpk@agilehead.com>
2020-04-03 18:58:06 +0530

    Fix build script

--- build-x86_64-linux-android.sh
+++ build-x86_64-linux-android.sh
@@ -1,8 +1,14 @@
-BASEDIR=$(dirname "$0")
+BASEDIR=$(dirname "$2")
 
 export OPENSSL_STATIC=1
-export OPENSSL_LIB_DIR=`realpath "$BASEDIR"/deps/openssl`
-export OPENSSL_INCLUDE_DIR=`realpath "$BASEDIR"/deps/openssl/include`
+export OPENSSL_LIB_DIR=`realpath "$BASEDIR"/../openssl_for_ios_and_android/output/openssl_111_android_build/x86_64/usr/local/lib`
+export OPENSSL_INCLUDE_DIR=`realpath "$BASEDIR"/../openssl_for_ios_and_android/output/openssl_111_android_build/x86_64/usr/local/include`
+
+# export OPENSSL_LIB_DIR=/home/jeswin/repos/jeswin/openssl_for_ios_and_android/output/openssl_111_android_build/x86_64/usr/local/lib
+# export OPENSSL_INCLUDE_DIR=/home/jeswin/repos/jeswin/openssl_for_ios_and_android/output/openssl_111_android_build/x86_64/usr/local/include
+
+# echo $OPENSSL_LIB_DIR
+# echo $OPENSSL_INCLUDE_DIR
 
 echo Compiling for x86_64-linux-android...
 cargo ndk --target x86_64-linux-android --android-platform 28 -- build --release
