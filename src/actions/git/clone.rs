7e58c491ce88503c6f3ae0c11548d0f1067ff350
Jeswin <jeswinpk@agilehead.com>
2020-04-04 09:23:08 +0530

    Test writes to sdcard

--- src/actions/git/clone.rs
+++ src/actions/git/clone.rs
@@ -1,4 +1,6 @@
-use git2::Repository;
+use git2::build::{CheckoutBuilder, RepoBuilder};
+use git2::{FetchOptions, Progress, RemoteCallbacks};
+use std::path::{Path, PathBuf};
 
 struct Repo<'a> {
   username: &'a str,
@@ -8,7 +10,16 @@ struct Repo<'a> {
 
 pub async fn clone_over_http<'a>(args: &str) -> Result<String, String> {
   let url = "https://github.com/alexcrichton/git2-rs";
-  let repo = Repository::clone(url, "/path/to/a/repo");
+  //let repo = Repository::clone(url, "/home/jeswin/temp/lalala");
   
-  Ok(r#"{ result: true }"#.to_owned())
+  let mut co = CheckoutBuilder::new();
+  let mut fo = FetchOptions::new();
+  //fo.remote_callbacks(cb);
+  RepoBuilder::new()
+      .fetch_options(fo)
+      .with_checkout(co)
+      //.clone(&args.arg_url, Path::new(&args.arg_path))?;
+      .clone(url, Path::new("/sdcard/boomer"));
+
+  Ok(r#"{ result: true, checked_out: 2 }"#.to_owned())
 }
